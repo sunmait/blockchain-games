87d08248a934c173c2cec948c78c4f692a852329
Vitaly <009_bidjy_885@mail.ru>
2018-05-23 13:19:15 +0300

    Added js client.

--- client/src/components/pages/MainPage/MainPage.jsx
+++ client/src/components/pages/MainPage/MainPage.jsx
@@ -1,8 +1,31 @@
 import React, { Component } from 'react';
 import smthg from '../../../assets/images/smthg.png';
 import './MainPage.css';
+import { contractAddress } from '../../../appSettings';
+import contractABI from '../../../smart-contracts-abis/GuessNumberGame';
 
 class MainPage extends Component {
+  constructor(props) {
+    super(props);
+
+    const contract = window.web3.eth.contract(contractABI.abi);
+
+    this.state = {
+      contractInstance: contract.at(contractAddress),
+    }
+  }
+
+  hostGame = () => {
+    const {hostGame} = this.state.contractInstance;
+    hostGame (1, (err, answer) => {
+      if (err) {
+        console.log('error', err);
+      } else {
+        console.log('Game mb even hosted', answer);
+      };
+    });
+  }
+
   render() {
     return (
       <div className="main-page">
@@ -11,6 +34,13 @@ class MainPage extends Component {
         </header>
         <div className="main-page-body">
           <img className="main-page-body-picture" src={smthg} alt="smthg" />
+        </div>
+        <div>
+          <button
+            onClick={this.hostGame}
+          >
+            Host game
+          </button>
         </div>
       </div>
     );
--- smart-contracts/truffle.js
+++ smart-contracts/truffle.js
@@ -1,4 +1,12 @@
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
+  contracts_build_directory: "../client/src/smart-contracts-abis",
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*",
+    }
+  }
 };
