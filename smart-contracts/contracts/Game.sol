eed57288f8b7f9c4c8f12a4cd2b8d1f4eebf30b2
denis.zhidis <denis_zhidis@mail.ru>
2018-05-25 11:16:47 +0300

    changed contract

--- smart-contracts/contracts/Game.sol
+++ smart-contracts/contracts/Game.sol
@@ -1,83 +1,82 @@
 pragma solidity ^0.4.23;
 
 contract GuessNumberGame {
+    enum Result { Unfinished, Win, Loss }
+    enum State { Unrealized, Hosted, Joined, Ended }
+
     struct Game {
         address player1;
         address player2;
+        bytes32 player1NumberHidden;
         uint8 player1Number;
         uint8 player2Number;
-        uint8 result;
+        State state;
+        Result result;
     }
 
     address public owner;
     uint gameIdCounter;
     Game[] public games;
 
-    event GameHosted(address player1, uint8 player1Number);
+    event GameHosted(address player1, bytes32 player1Number, uint indexed gameId);
     event GameJoined(address player2, uint8 player2Number);
-    event GameEnded(address player1, address player2, uint result);
+    event GameEnded(address player1, address player2, Result result);
+
+    modifier onlyOwner {require(msg.sender == owner); _;}
 
     constructor() public {
         owner = msg.sender;
     }
 
-    function getGames() public view returns (uint[]) {
-        uint[] memory ids = new uint[](games.length);
-        for (uint i = 0; i < games.length; i++) {
-            ids[i] = i;
-        }
-        return ids;
+    function getGames() view internal returns (Game[]) {
+        return games;
     }
 
-    function hostGame(uint8 move) public returns (bool success) {
-        require(move > 0 && move < 3);
-        Game memory game = Game(msg.sender, address(0), move, 0, 0);
-        games.push(game);
+    function hostGame(bytes32 move) public returns (uint gameId) {
+        games.push(Game(msg.sender, address(0), move, 0, 0, State.Hosted, Result(0)));
+        gameId = gameIdCounter;
 
-        emit GameHosted(msg.sender, move);
+        emit GameHosted(msg.sender, move, gameId);
 
         gameIdCounter++;
-
-        return true;
     }
 
     function joinGame(uint gameId, uint8 move) public returns (bool success) {
         Game storage thisGame = games[gameId];
-        require(move > 0 && move < 3);
+        require(thisGame.state == State.Hosted);
+        require(move > 0 && move <= 10);
         thisGame.player2 = msg.sender;
         thisGame.player2Number = move;
+        thisGame.state = State.Joined;
 
         emit GameJoined(msg.sender, move);
 
         return true;
     }
 
-    function revealMove(uint gameId) public returns (bool success) {
+    function revealMove(uint gameId, uint8 move, string secret) public returns (Result result) {
         Game storage thisGame = games[gameId];
 
-        if (thisGame.player1Number == thisGame.player2Number) {
-            thisGame.result = 1;
+        require(thisGame.state == State.Joined);
+        require(thisGame.player1 == msg.sender);
+        require(thisGame.player1NumberHidden == keccak256(uint(move), secret));
+        thisGame.player1Number = move;
+
+        if (move % 2 == thisGame.player2Number % 2) {
+            result = Result.Loss;
         } else {
-            thisGame.result = 2;
+            result = Result.Win;
         }
+
+
+        thisGame.result = result;
 
         emit GameEnded(thisGame.player1, thisGame.player2, thisGame.result);
 
-        return true;
     }
 
-    function getResult(uint gameId) public view returns (uint8) {
-        Game storage thisGame = games[gameId];
-        return thisGame.result;
-    }
-
-    function getHostValue(uint gameId) public view returns(uint) {
-        Game storage thisGame = games[gameId];
-        return thisGame.player1Number;
-    }
-
-    function getJoinedValue(uint gameId) public view returns(uint) {
-        Game storage thisGame = games[gameId];
-        return thisGame.player2Number;
+    function setOwner(address newOwner) public {
+        require(msg.sender == owner);
+        owner = newOwner;
     }
 }
