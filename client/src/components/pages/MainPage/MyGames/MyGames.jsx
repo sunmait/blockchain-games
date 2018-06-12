634317b549ea1afaf8b6958362c7d7fd2377bffe
Vitaly <009_bidjy_885@mail.ru>
2018-06-12 14:02:17 +0300

    Configured windrawl functionality on client.

--- client/src/components/pages/MainPage/MyGames/MyGames.jsx
+++ client/src/components/pages/MainPage/MyGames/MyGames.jsx
@@ -1,40 +1,9 @@
 import React from 'react';
 import Row from 'react-bootstrap/lib/Row';
 import Col from 'react-bootstrap/lib/Col';
-import Button from 'react-bootstrap/lib/Button';
-import {mapVeiToEth} from '../../../../helpers/ethConverter';
+import MyGamesItemContainer from './MyGamesItemContainer';
 
 class MyGames extends React.Component {
-  renderRevealButton = (item) => {
-    if (item.status === 'Joined') {
-      return (
-        <Button
-          className="pull-right"
-          onClick={() => {
-            this.props.handleCurrentGameChange(item.id);
-            this.props.handleActiveTabChange(3);
-          }}
-        >
-          Reveal
-        </Button>
-      );
-    }
-    if (item.status === 'Ended') {
-      return (
-        <Button
-          className="pull-right"
-          onClick={() => {
-            this.props.handleCurrentGameChange(item.id);
-            this.props.handleActiveTabChange(3);
-          }}
-        >
-          Check result
-        </Button>
-      )
-    }
-    return null;
-  };
-
   renderGamesList = () => {
     if (this.props.userGamesList.length === 0) {
       return (
@@ -44,25 +13,12 @@ class MyGames extends React.Component {
       );
     }
     return this.props.userGamesList.map((item, index) => {
-      const key = index;
-      const risk = (
-        <React.Fragment>
-          ({(this.props.ethPrice * mapVeiToEth(item.price)).toFixed(2)}USD)
-        </React.Fragment>
-      );
       return (
-        <Row key={key} className="games-list-element-container">
-          <Col md={6}>
-            Game #{item.id}
-            <br />
-            Game price: {mapVeiToEth(item.price)} ETH {this.props.ethPrice ? risk : null}
-          </Col>
-          <Col md={6}>
-            Game status: {item.status}
-            {this.renderRevealButton(item)}
-          </Col>
-        </Row>
-      );
+        <MyGamesItemContainer
+          item={item}
+          key={index}
+        />
+      )
     });
   };
 
---
+++ client/src/components/pages/MainPage/MyGames/MyGamesItem.jsx
@@ -0,0 +1,123 @@
+import React from 'react';
+import Button from 'react-bootstrap/lib/Button';
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
+import {mapVeiToEth} from '../../../../helpers/ethConverter';
+import Countdown from '../../../common/Countdown/Countdown';
+
+class MyGamesItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isFinishExpiredGameButton: false,
+    };
+  }
+
+  renderRevealButton = (item) => {
+    if (item.status === 'Joined') {
+      if (this.props.currentAccount === item.hostAddress) {
+        return (
+          <React.Fragment>
+            <Col md={12}>
+              <Button
+                className="pull-right"
+                onClick={() => {
+                  this.props.handleCurrentGameChange(item.id);
+                  this.props.handleActiveTabChange(3);
+                }}
+              >
+                Reveal
+              </Button>
+            </Col>
+            <Col md={12}>
+              <Countdown
+                start={item.gameJoinTime}
+                className="pull-right"
+              />
+            </Col>
+          </React.Fragment>
+        );
+      }
+      if (this.props.currentAccount === item.joinedAddress) {
+        return (
+          <React.Fragment>
+            <Col md={12}>
+              <Button
+                className="pull-right"
+                onClick={() => this.finishExpiredGame(item.id)}
+                disabled={!this.state.isFinishExpiredGameButton}
+              >
+                Finish game
+              </Button>
+            </Col>
+            <Col md={12}>
+              <Countdown
+                start={item.gameJoinTime}
+                countdownEnded={this.enableFinishExpiredGameButton}
+                className="pull-right"
+              />
+            </Col>
+          </React.Fragment>
+        );
+      }
+    }
+    if (item.status === 'Ended') {
+      return (
+        <Button
+          className="pull-right"
+          onClick={() => {
+            this.props.handleCurrentGameChange(item.id);
+            this.props.handleActiveTabChange(3);
+          }}
+        >
+          Check result
+        </Button>
+      )
+    }
+    return null;
+  };
+
+  enableFinishExpiredGameButton = () => {
+    this.setState({
+      isFinishExpiredGameButton: true,
+    });
+  };
+
+  finishExpiredGame = (gameId) => {
+    const {withdrawal} = this.props.contractInstance;
+    withdrawal(gameId,
+      (err, answer) => {
+        if (err) {
+          console.log('err: ', err);
+        } else {
+          console.log('answer: ', answer);
+        }
+      });
+  };
+
+  render() {
+    const risk = (
+      <React.Fragment>
+        ({(this.props.ethPrice * mapVeiToEth(this.props.item.price)).toFixed(2)}USD)
+      </React.Fragment>
+    );
+    return (
+      <Row className="games-list-element-container">
+        <Col md={6}>
+          Game #{this.props.item.id}
+          <br />
+          Game price: {mapVeiToEth(this.props.item.price)} ETH {this.props.ethPrice ? risk : null}
+        </Col>
+        <Col md={3}>
+          Game status: {this.props.item.status}
+        </Col>
+        <Col md={3}>
+          {this.renderRevealButton(this.props.item)}
+        </Col>
+      </Row>
+    );
+  }
+}
+
+export default MyGamesItem;
